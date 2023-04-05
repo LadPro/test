7bf8bdd206ba21e86c71937d5291fb7364c80cd5
Joel Mora <joeljmorab@gmail.com>
2023-04-05 16:47:11 -0400

    primer commit

---
+++ code.js
@@ -0,0 +1,7 @@
+"use strict"
+
+/////////////////////////service worker es un intermediario entre el servidor web y el navegador que puede realizar funciones
+
+// console.log(navigator.serviceWorker)
+
+if (navigator.serviceWorker){}
